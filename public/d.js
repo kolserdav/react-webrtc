b7bc9d50de4e9931bfa221316a9fcca3122c4e86
kolserdav <serega12101983@gmail.com>
2022-06-02 13:55:38 +0700

    Save experiments

--- public/d.js
+++ public/d.js
@@ -4,35 +4,41 @@ if (navigator.mediaDevices) {
   console.log('getUserMedia supported.');
 
   const constraints = { audio: true, video: true };
-  const chunks = [];
+  let timeout = setInterval(() => {
+    /** */
+  }, Infinity);
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then((stream) => {
-      /**
-       * 
-    video/webm
-    video/webm;codecs=vp8
-    video/webm;codecs=vp9
-    video/webm;codecs=vp8.0
-    video/webm;codecs=vp9.0
-    video/webm;codecs=h264
-    video/webm;codecs=H264
-    video/webm;codecs=avc1
-    video/webm;codecs=vp8,opus
-    video/WEBM;codecs=VP8,OPUS
-    video/webm;codecs=vp9,opus
-    video/webm;codecs=vp8,vp9,opus
-    video/webm;codecs=h264,opus
-    video/webm;codecs=h264,vp9,opus
-
-    video/x-matroska;codecs=avc1
-
-    audio/webm
-    audio/webm;codecs=opus
-
-       */
-
-      const mimeType = 'video/webm';
+      const codecs = [
+        'video/webm',
+        'video/webm;codecs=vp8',
+        'video/webm;codecs=vp9',
+        'video/webm;codecs=vp8.0',
+        'video/webm;codecs=vp9.0',
+        'video/webm;codecs=h264',
+        'video/webm;codecs=H264',
+        'video/webm;codecs=avc1',
+        'video/webm;codecs=vp8,opus',
+        'video/WEBM;codecs=VP8,OPUS',
+        'video/webm;codecs=vp9,opus',
+        'video/webm;codecs=vp8,vp9,opus',
+        'video/webm;codecs=h264,opus',
+        'video/webm;codecs=h264,vp9,opus',
+        'video/x-matroska;codecs=avc1',
+      ];
+      let mimeType = '';
+      for (let i = 0; codecs[i]; i++) {
+        const item = codecs[i];
+        if (MediaRecorder.isTypeSupported(item) && MediaSource.isTypeSupported(item)) {
+          console.log('Supported mimetype is', item);
+          mimeType = item;
+          break;
+        }
+      }
+      if (!mimeType) {
+        console.warn('From all list', codecs, 'not one is supported');
+      }
       const mediaRecorder = new MediaRecorder(stream, {
         mimeType,
       });
@@ -53,6 +59,7 @@ if (navigator.mediaDevices) {
         console.log('recorder stopped');
         record.style.background = '';
         record.style.color = '';
+        clearInterval(timeout);
       };
 
       mediaRecorder.onstart = () => {
@@ -63,21 +70,29 @@ if (navigator.mediaDevices) {
         video.src = newObjectUrl;
         mediaSource.addEventListener('sourceopen', sourceOpen);
         function sourceOpen(_) {
+          const queue = [];
           const buffer = mediaSource.addSourceBuffer(mimeType);
           buffer.addEventListener('update', () => {
-            console.log(1);
+            if (queue.length > 0 && !buffer.updating) {
+              buffer.appendBuffer(queue.shift());
+            }
           });
+
           mediaRecorder.ondataavailable = async function (e) {
-            e.data.arrayBuffer().then((data) => {
-              buffer.appendBuffer(data);
-            });
+            if (e.data.size > 0) {
+              e.data.arrayBuffer().then((data) => {
+                const uIntArray = new Uint8Array(data);
+                if (!buffer.updating) {
+                  buffer.appendBuffer(uIntArray);
+                } else {
+                  queue.push(Uint8Array);
+                }
+              });
+            }
           };
-          setInterval(() => {
+          timeout = setInterval(() => {
             mediaRecorder.requestData();
           }, 10);
-          setTimeout(() => {
-            video.play();
-          });
         }
       };
     })
